e433980dee21401ea99cc6b12622c6b7c6d16e46
Rudra-Prajapati1 <prajapatirudra817@gmail.com>
2025-12-12 23:15:09 +0530

    started group card

--- src/components/ShowMoreButton.jsx
+++ src/components/ShowMoreButton.jsx
@@ -1,10 +1,16 @@
 import { ArrowRight } from "lucide-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const ShowMore = ({ text = "" }) => {
+const ShowMore = ({ text = "", path= "/" }) => {
+  const navigate = useNavigate();
+
   return (
     <button
       type="button"
+      onClick={() => {
+        navigate(path);
+      }}
       className="mt-20 flex gap-2 items-center border-3 text-primary border-primary px-2 py-3 font-semibold font-inter rounded-xl hover:scale-105 group cursor-pointer duration-300"
     >
       {text}
--- src/components/groups/GroupCard.jsx
+++ src/components/groups/GroupCard.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import group_temp_image from "/images/group_temp_image.jpg";
 
 const GroupCard = () => {
-  return <div>GroupCard</div>;
+  return (
+    <div
+      className="border-3 border-primary text-primary font-inter py-6 px-4 rounded-2xl
+    w-full max-w-[20rem] mx-auto"
+    >
+      <img
+        src={group_temp_image}
+        alt=""
+        className="rounded-full h-40 w-40 object-cover"
+      />
+    </div>
+  );
 };
 
 export default GroupCard;
