ad03cf634356d91f1aa0f7ccea80cc14c9c13bc9
Rudra-Prajapati1 <prajapatirudra817@gmail.com>
2025-12-14 22:09:32 +0530

    Created notesSlice and QuestionsSlice and also created few necessary tables in Mockman with sample documents, TESTING is remaining

---
+++ src/redux/slice/notesSlice.js
@@ -0,0 +1,66 @@
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
+import api from "../../api/axios";
+
+const notesAdapter = createEntityAdapter({
+  selectId: (note) => note.N_ID,
+  sortComparer: (a, b) => new Date(b.Added_On) - new Date(a.Added_On),
+});
+
+const initialState = notesAdapter.getInitialState({
+  status: "idle",
+  error: null,
+});
+
+export const fetchNotes = createAsyncThunk(
+  "notes/fetchNotes",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await api.get(
+        `/collections/${
+          import.meta.env.VITE_MOCKMAN_API_KEY
+        }/693edcbeb6dbbbd14cf90d07/documents`
+      );
+
+      return data.map((d) => d.data);
+    } catch (error) {
+      return rejectWithValue(error?.message || "Failed to fetch Notes.");
+    }
+  }
+);
+
+export const notesSlice = createSlice({
+  name: "notes",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchNotes.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(fetchNotes.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        notesAdapter.setAll(state, action.payload);
+      })
+      .addCase(fetchNotes.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload || action.error.message;
+      });
+  },
+});
+
+export const {
+  selectAll: selectAllNotes,
+  selectById: selectNotesById,
+  selectIds: selectNotesIds,
+  selectEntities: selectNotesEntities,
+} = notesAdapter.getSelectors((state) => state.notes);
+
+export const selectNotesStatus = (state) => state.notes.status;
+export const selectNotesError = (state) => state.notes.error;
+
+export default notesSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import eventsReducer from "./slice/eventsSlice";
 import chatRoomsReducer from "./slice/chatRoomsSlice";
+import notesReducer from "./slice/notesSlice";
+import questionsReducer from "./slice/questionsSlice";
 
 export default configureStore({
   reducer: {
     events: eventsReducer,
     chatRooms: chatRoomsReducer,
+    notes: notesReducer,
+    questions: questionsReducer,
   },
 });
