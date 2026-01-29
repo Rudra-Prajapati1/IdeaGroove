6322b8f00b31286302d2d182346d7962c9ee9467
Sejal2407 <sejalokhatri@gmail.com>
2026-01-30 03:01:36 +0530

    Social Links

--- src/links/socialLinks.js
+++ src/links/socialLinks.js
@@ -1,7 +1,6 @@
 import { Facebook, Instagram, Twitter, X } from "lucide-react";
 
 export const socialLinks = [
-  { id: 1, title: "Twitter", icon: Twitter },
-  { id: 2, title: "Instagram", icon: Instagram },
-  { id: 3, title: "Facebook", icon: Facebook },
+  { id: 1, title: "Twitter", icon: Twitter,href:"https://www.instagram.com/theideagroove/" },
+  { id: 2, title: "Instagram", icon: Instagram, href:"https://x.com/theideagroove" },
 ];
