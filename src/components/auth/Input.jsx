10fc64697f12b5f0aae2bd868e84d210103200a1
Rudra-Prajapati1 <prajapatirudra817@gmail.com>
2026-01-29 21:43:47 +0530

    Signup error message

--- src/components/auth/Input.jsx
+++ src/components/auth/Input.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-const Input = ({ label, type = "text", placeholder, value, onChange }) => (
+const Input = ({
+  label,
+  type = "text",
+  placeholder,
+  value,
+  onChange,
+  className = "",
+  name,
+}) => (
   <label className="text-md font-semibold text-primary">
     {label}:
     <input
       type={type}
+      name={name}
       value={value}
       placeholder={placeholder}
       onChange={(e) => onChange(e.target.value)}
-      className="w-full text-sm border-2 border-gray-300 rounded-xl p-3 mt-1 focus:border-primary/60 outline-none"
+      className={`w-full text-sm border-2 rounded-xl p-3 mt-1 focus:border-primary/60 outline-none ${className}`}
       required
     />
   </label>
